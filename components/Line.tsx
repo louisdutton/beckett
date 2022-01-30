f6098b4a070168e5fe321c7c49841670007dd0ed
Louis Dutton <clouisdutton@gmail.com>
2022-01-30 19:21:31 +0000

    setup change handler for line input

--- components/Line.tsx
+++ components/Line.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import Textarea from "react-autosize-textarea";
 import { useAppDispatch, useAppSelector } from "../lib/hooks";
 import { add, remove } from "../lib/reducers/blocks";
@@ -16,20 +16,22 @@ const Line = () => {
 	} = useAppSelector((state) => state);
 	const dispatch = useAppDispatch();
 	const inputRef = useRef<HTMLTextAreaElement>(null);
+	const [input, setInput] = useState("");
 	const [character, setCharacter] = useState(0);
 
 	const keyHandler = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
 		switch (e.code) {
 			case "Enter": {
-				dispatch(add());
 				e.preventDefault();
+				if (inputRef.current?.value !== "") {
+					dispatch(add());
+				}
 				break;
 			}
 			case "Backspace": {
 				if (blocks.length > 1 && e.metaKey) dispatch(remove());
 				break;
 			}
-			// TODO: implement charcter switching key bindings
 			case "ArrowUp": {
 				if (e.altKey) {
 					setCharacter((c) => mod(c - 1, characters.length));
@@ -47,12 +49,11 @@ const Line = () => {
 		}
 	};
 
+	const handleChange = (e: FormEvent<HTMLTextAreaElement>) => {};
+
 	return (
 		<div className="relative">
-			{/* <div className="w-[1.5in] print:w-[2in] peer-focus:bg-black">
-				<Dropdown<string> options={characters} />
-			</div> */}
-			<fieldset className="transition-all border-2 rounded-lg group border-slate-200 bg-slate-100 character-field  focus-within:scale-[103%] focus-within:shadow-lg ease-in-out focus-within:bg-white focus-within:border-blue-400 text-slate-400 focus-within:text-blue-400">
+			<fieldset className="print:hidden transition-all border-2 rounded-lg group border-slate-200 bg-slate-100 character-field  focus-within:scale-[103%] focus-within:shadow-lg ease-in-out focus-within:bg-white focus-within:border-blue-400 text-slate-400 focus-within:text-blue-400">
 				<legend className="px-2 ml-4 font-bold uppercase border-black">
 					{characters[character].name}
 				</legend>
@@ -60,11 +61,17 @@ const Line = () => {
 					ref={inputRef}
 					className="flex-1 w-full px-5 pb-1 text-black bg-transparent resize-none focus:outline-none print:p-0 placeholder:text-slate-400"
 					onKeyDown={keyHandler}
-					// onChange={handleChange}
+					onChange={handleChange}
 					placeholder="..."
 					autoFocus={true}
 				/>
 			</fieldset>
+			{inputRef.current?.value && (
+				<div className="hidden print:flex">
+					<p className="uppercase w-[2in]">{characters[character].name}:</p>
+					<p>{inputRef.current?.value}</p>
+				</div>
+			)}
 		</div>
 	);
 };
