7ad71ee5a5b6cdd128aecac67df8359c42a5ccc4
Louis Dutton <clouisdutton@gmail.com>
2022-01-30 19:21:01 +0000

    lightened block buttons

--- components/BlockButtons.tsx
+++ components/BlockButtons.tsx
@@ -9,12 +9,12 @@ const BlockButtons = () => {
 		<div className="flex flex-col gap-2 sm:flex-row print:hidden justify-evenly">
 			<button
 				onClick={() => dispatch(add())}
-				className="flex items-center justify-center w-full gap-2 px-5 py-3 text-white uppercase transition-colors rounded shadow-md bg-primary-500 hover:bg-primary-600">
+				className="flex items-center justify-center w-full gap-2 px-5 py-3 text-white uppercase transition-colors rounded shadow-md bg-primary-400 hover:bg-primary-500">
 				<Chat size={28} weight="fill" /> Line
 			</button>
 			<button
 				onClick={() => dispatch(add())}
-				className="flex items-center justify-center w-full gap-2 px-5 py-3 uppercase transition-colors border-2 rounded text-primary-500 border-primary-500 hover:border-primary-600">
+				className="flex items-center justify-center w-full gap-2 px-5 py-3 uppercase transition-colors border-2 rounded text-primary-400 border-primary-400 hover:border-primary-500">
 				<Sword size={28} /> Action
 			</button>
 		</div>
